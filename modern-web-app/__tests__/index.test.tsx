13b78c0291d4c5cf8f9ad8b4007cf149974bc248
colmanfranco <colmanfm@gmail.com>
2022-08-20 20:57:57 +0200

    fixes tests

--- modern-web-app/__tests__/index.test.tsx
+++ modern-web-app/__tests__/index.test.tsx
@@ -6,7 +6,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to nex\.js!/i,
+      name: /welcome to next\.js!/i,
     })
 
     expect(heading).toBeInTheDocument()
